a393509d8586799fea2985c9fc7c46774320c619
Dungyy <erickmunoz13@gmail.com>
2025-05-15 02:36:21 -0500

    forgot other files\

---
+++ logger.js
@@ -0,0 +1,16 @@
+import winston from 'winston';
+
+// Initialize Winston logger
+export const LOG = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `[${timestamp}] ${level}: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
